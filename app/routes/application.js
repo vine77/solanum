a53d8c29c5d07d0606b207e31a1d33284504740c
Nathan Ward <ward@digivine.com>
2016-10-22 16:44:48 -0700

    Demonstrate adding and deleting records

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,5 +5,19 @@ export default Ember.Route.extend({
 
   model() {
     return this.get('store').findAll('pomodoro')
+  },
+
+  actions: {
+    add() {
+      const newPomodoro = this.get('store').createRecord('pomodoro', {
+        title: 'Create Pomodoro app'
+      })
+      return newPomodoro.save()
+    },
+
+    delete() {
+      const pomodoros = this.get('store').peekAll('pomodoro')
+      return pomodoros.get('lastObject').destroyRecord()
+    }
   }
 })
